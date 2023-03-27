fdb10b94e3daa3eb374649c8457f1c348dd76236
Hockey86 <hockeysun86@gmail.com>
2023-03-26 20:13:13 -0400

    writing model again

--- model_scripts/models.py
+++ model_scripts/models.py
@@ -1,10 +1,15 @@
+from collections import deque
 import pickle
-from torch import nn
+import numpy as np
 import torch as th
+from torch import nn
 from torch.utils.data import Dataset
+from torch.distributions import Beta, Bernoulli
+from torch.optim import Adam
+import lightning.pytorch as pl
 
 
-class OOSSDatset(Dataset):
+class OOSSDataset(Dataset):
     """
     sid: subject id
     X: signal or feature
@@ -16,73 +21,145 @@ class OOSSDatset(Dataset):
         self.path = path
         with open(path, 'rb') as ff:
             res = pickle.load(ff)
-        self.sids = res['sids']
         self.X = res['X']; self.Xnames = res['Xnames']
-        self.S = res['S']
         self.Y = res['Y']
+        self.T = np.array([len(x) for x in res['S']])
+        #self.S = res['S']
         #self.L = res['L']; self.Lnames = res['Lnames']
+        #self.sids = res['sids']
+        self.var_names = ['X', 'Y', 'T']#, 'S', 'L'
+        self.var2difflen = {'X':True, 'Y':False, 'T':False}
+        self.var2type = {'X':'torch.FloatTensor', 'Y':'torch.IntTensor', 'T':'torch.FloatTensor'}
+
+        # only take N2 and N3
+        self.X = [x[s<=2] for x,s in zip(self.X, res['S'])]
+
+        # squeeze [0,1] to (0,1)
+        N = self.T.sum()
+        for i in range(len(self.X)):
+            self.X[i][self.X[i]==0] = 0.5/N
+            self.X[i][self.X[i]==1] = (N-0.5)/N
 
     def __getitem__(self, idx):
-        return {'sid':self.sids[idx],
-                'X':self.X[idx],
-                'S':self.S[idx],
-                'Y':self.Y[idx],
-                #'L':self.L[idx],
-                }
+        return {x:getattr(self, x)[idx] for x in self.var_names}
 
     def __len__(self):
-        return len(self.sids)
+        return len(self.T)
+
+    def collate_fn(self, batch):
+        res = {}
+        for x in self.var_names:
+            tp = self.var2type[x]
+            if type(batch)==list:
+                if self.var2difflen[x]:
+                    # pad to form masked tensor
+                    res_raw = [y[x] for y in batch]
+                    maxT = max([len(y) for y in res_raw])
+                    res[x] = th.tensor(np.array([np.pad(y, ((0,maxT-len(y)),(0,0)), constant_values=0.5) for y in res_raw])).type(tp)
+                    res[x+'_mask'] = th.tensor(np.array([np.pad(np.ones(len(y)), ((0,maxT-len(y)),)) for y in res_raw])).float()
+                    #res[x] = tensor(val, mask)
+                else:
+                    res[x] = th.tensor([y[x] for y in batch]).type(tp)
+            else:
+                res[x] = th.tensor(batch[x]).type(tp)
+        return res
 
 
 class OOSSNet(pl.LightningModule):
-    def __init__(self):
+    """
+    """
+    def __init__(self, lr=1e-3, n_MCMC=100, baseline_len=0):
         super().__init__()
         self.save_hyperparameters()
+        self.previous_f = deque(maxlen=self.hparams.baseline_len)
 
-        self.encoder = 
-        self.decoder = 
+        # q(Z|S,X,L)
+        self.qz_logit_thres = nn.Parameter(th.randn(()))
+        self.qz_log_slope = nn.Parameter(th.randn(()))
 
-    def forward(self, x):
-        return self.encoder(x)
+        # p(X|Z)
+        self.px_log_alpha = nn.Parameter(th.randn((2,)))
+        self.px_log_beta = nn.Parameter(th.randn((2,)))
 
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-4)
-
-    def myloss(self, x_hat, logscale, x):
-        return
+        # p(Z|Y,L)
+        self.pz_log_alpha = nn.Parameter(th.randn((2,)))
+        self.pz_log_beta = nn.Parameter(th.randn((2,)))
 
     def training_step(self, batch, batch_idx):
-        x, _ = batch
+        return self._forward(batch, batch_idx, 'train')
+    def validation_step(self, batch, batch_idx):
+        self._forward(batch, batch_idx, 'validation')
+    def test_step(self, batch, batch_idx):
+        self._forward(batch, batch_idx, 'test')
+    def predict_step(self, batch, batch_idx, dataloader_idx=0):
+        return self(batch)
+    def configure_optimizers(self):
+        return Adam(self.parameters(), lr=self.hparams.lr)
 
-        # encode x to get the mu and variance parameters
-        x_encoded = self.encoder(x)
-        mu, log_var = self.fc_mu(x_encoded), self.fc_var(x_encoded)
+    def forward(self, x, sigmoid=True):
+        # q(Z|S,X,L)
+        thres = th.sigmoid(self.qz_logit_thres)
+        slope = th.exp(self.qz_log_slope)
+        logit_pz = slope * (x[...,0] - thres)#TODO remove [,0]
+        if sigmoid:
+            return th.sigmoid(logit_pz)
+        else:
+            return logit_pz
 
-        # sample z from q
-        std = torch.exp(log_var / 2)
-        q = torch.distributions.Normal(mu, std)
-        z = q.rsample()
+    def _forward(self, batch, batch_idx, prefix):
+        X, Xmask, Y, T = batch['X'], batch['X_mask'], batch['Y'], batch['T']
 
-        # decoded
-        x_hat = self.decoder(z)
+        # encode: q(Z|S,X,L)
+        logit_qz = self(X, sigmoid=False)
 
-        # reconstruction loss
-        recon_loss = self.gaussian_likelihood(x_hat, self.log_scale, x)
+        # sample z from log_pz
+        px_alpha = th.exp(self.px_log_alpha)
+        px_beta  = th.exp(self.px_log_beta)
+        z_dist_q = Bernoulli(logits=logit_qz)
+        z_mcmc = z_dist_q.sample((self.hparams.n_MCMC,)).data
+        log_qz = z_dist_q.log_prob(z_mcmc)
+        log_qz = (log_qz*Xmask).sum(dim=-1)
 
-        # kl
-        kl = self.kl_divergence(z, mu, std)
+        # decode: p(X|Z)
+        x_dist = Beta(
+            px_alpha.index_select(0, z_mcmc.int().flatten()).reshape(z_mcmc.shape),
+            px_beta.index_select(0, z_mcmc.int().flatten()).reshape(z_mcmc.shape))
+        log_px = x_dist.log_prob(X[...,0])#TODO
+        log_px = (log_px*Xmask).sum(dim=-1)
 
-        # elbo
-        elbo = (kl - recon_loss)
-        elbo = elbo.mean()
+        # regularizer: p(Z|Y,L)
+        pz_alpha = th.exp(self.pz_log_alpha)
+        pz_beta  = th.exp(self.pz_log_beta)
+        z_dist_p = Beta(pz_alpha.index_select(0,Y), pz_beta.index_select(0,Y))
+        log_pz = z_dist_p.log_prob((z_mcmc*Xmask).sum(dim=-1)/T)
+        
+        fs = log_px + log_pz - log_qz
+
+        # get loss
+        if len(self.previous_f)==0:
+            baseline = 0
+        else:
+            baseline = th.Tensor(self.previous_f).mean()
+        loss = -th.mean(log_qz*(fs.data - baseline) + fs)
+        if prefix=='train':
+            self.previous_f.append(fs.data.mean())
 
         self.log_dict({
-            'elbo': elbo,
-            'kl': kl.mean(),
-            'recon_loss': recon_loss.mean(),
-            'reconstruction': recon_loss.mean(),
-            'kl': kl.mean(),
+            prefix+'_loss': loss,
+            prefix+'_rec_log_px':log_px.mean(),
+            prefix+'_D_kl_qz_pz':log_qz.mean()-log_pz.mean(),
         })
 
-        return elbo
+        return loss
+
+    def on_train_start(self):
+        self.previous_f.clear()
+    def on_validation_start(self):
+        self.previous_f.clear()
+    def on_test_start(self):
+        self.previous_f.clear()
+    def on_predict_start(self):
+        self.previous_f.clear()
+    def on_fit_start(self):
+        self.previous_f.clear()
 
--- step2_case_study/fit_model.py
+++ step2_case_study/fit_model.py
@@ -1,14 +1,13 @@
 from argparse import ArgumentParser
+import os
+import json
 import numpy as np
+import torch as th
+from torch.utils.data import Subset, DataLoader
+import lightning.pytorch as pl
 import sys
 sys.path.insert(0, '../model_scripts')
-from sklearn.model_selection import KFold
-import torch as th
-th.backends.cudnn.determinstic = True
-th.backends.cudnn.benchmark = False
 from models import OOSSNet, OOSSDataset
-import pytorch_lightning as pl
-from lightning import Trainer
 
 
 def get_data(dataset, Ncv):
@@ -20,43 +19,68 @@ def get_data(dataset, Ncv):
     data_tr = []; data_va = []; data_te = []
     for cvi in range(Ncv):
         teids = cv_ids[cvi]
-        not_teids = np.setdiff1d(np.arange(Ncv), cvi)
-        vaids = cv_ids[not_teids[-1]]
+        not_te_foldids = np.setdiff1d(np.arange(Ncv), cvi)
+        vaids = cv_ids[not_te_foldids[-1]]
         trids = np.setdiff1d(np.arange(len(dataset)), np.r_[vaids, teids])
 
-        dtr = Subset(dataset, trids)
-        dva = Subset(dataset, vaids)
-        dte = Subset(dataset, teids)
+        data_tr.append( Subset(dataset, trids) )
+        data_va.append( Subset(dataset, vaids) )
+        data_te.append( Subset(dataset, teids) )
 
-        data_tr.append(DataLoader(dtr))
-        data_va.append(DataLoader(dva))
-        data_te.append(DataLoader(dte))
     return data_tr, data_va, data_te
 
 
-def main(data_path, Ncv=5, use_gpu=False, verbose=True):
+def main(data_path, Ncv=5, use_gpu=False, verbose=True, **hp):#, ckpt_path='checkpoints'
+    #os.makedirs(ckpt_path, exist_ok=True)
     dall = OOSSDataset(data_path)
     dtrs, dvas, dtes = get_data(dall, Ncv)
 
+    sleep_stages_new = []
     for cvi, (dtr, dva, dte) in enumerate(zip(dtrs, dvas, dtes)):
         if verbose:
-            print(f'\n========\nCross validation fold {cvi+1}/{Ncv}\n========\n')
+            print(f"""
+=========================
+Cross validation fold {cvi+1}/{Ncv}
+=========================
+""")
 
-        model = OOSSNet()
-        trainer = Trainer()
-        trainer.fit(model, dtr, dva, dte)
+        dtr_loader = DataLoader(dtr, batch_size=hp['batch_size'], shuffle=True, collate_fn=dall.collate_fn)
+        dva_loader = DataLoader(dva, batch_size=hp['batch_size'], shuffle=False, collate_fn=dall.collate_fn)
+        dte_loader = DataLoader(dte, batch_size=hp['batch_size'], shuffle=False, collate_fn=dall.collate_fn)
+
+        model = OOSSNet(
+                lr=hp['lr'], n_MCMC=hp['n_MCMC'],
+                baseline_len=hp['baseline_len']
+                )
+        trainer = pl.Trainer(
+                accelerator='gpu' if use_gpu else 'cpu',
+                max_epochs=hp['max_epoch'],
+                deterministic=True, log_every_n_steps=5,
+                )
+        trainer.fit(model, train_dataloaders=dtr_loader, val_dataloaders=dva_loader)#, ckpt_path=ckpt_path)
+        import pdb;pdb.set_trace()
+
+        trainer.test(model, dataloaders=dte_loader, ckpt_path='best')
+        sleep_stages_new.append( trainer.predict(model, dte_loader, ckpt_path='best') )
 
 
 if __name__=='__main__':
     parser = ArgumentParser(description='SWA percent for dementia')
     parser.add_argument('-d', '--datapath')
+    parser.add_argument('-p', '--hyperparam', default='hyperparams.json')
+    parser.add_argument('-k', '--checkpointpath', default='checkpoints')
     parser.add_argument('-c', '--Ncv', default=5)
     parser.add_argument('-s', '--seed', default=2023)
     parser.add_argument('-v', '--verbose', default=True)
     parser.add_argument('-g', '--gpu', action='store_true')
     args = parser.parse_args()
-    import pdb;pdb.set_trace()
-    pl.seed_everything(args.seed)
 
-    main(args.datapath, Ncv=args.Ncv, use_gpu=args.gpu, verbose=args.verbose)
+    with open(args.hyperparam, 'r') as f:
+        hyperparam = json.load(f)
+    pl.seed_everything(args.seed)
+    main(
+        args.datapath, Ncv=args.Ncv, 
+        use_gpu=args.gpu, verbose=args.verbose,
+        #ckpt_path=args.checkpointpath,
+        **hyperparam)
 
